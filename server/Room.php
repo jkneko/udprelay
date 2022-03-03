103c712d5e1a7be32bb6e89b6255d2f286bf385a
kaneko <kaneko@mastershen>
2022-03-03 21:11:45 +0900

    インデント調整

--- server/Room.php
+++ server/Room.php
@@ -5,43 +5,43 @@ spl_autoload_register(function ($class_name) {
 
 class Room
 {
-    // ルームID
-    private $id;
+  // ルームID
+  private $id;
 
-    // 入室しているユーザ
-    private $users = [];
+  // 入室しているユーザ
+  private $users = [];
 
-    // constractor
-    public function __construct($room_id)
-    {
-        $this->id = $room_id;
-        echo('Room #' . $this->id . ' created.' . PHP_EOL);
+  // constractor
+  public function __construct($room_id)
+  {
+    $this->id = $room_id;
+    echo('Room #' . $this->id . ' created.' . PHP_EOL);
+  }
+
+  // 入室
+  public function join($user)
+  {
+    $this->users[$user->address] = $user;
+    echo("[join] {$user->address} joined into #{$this->id}\n");
+    echo(count($this->users) . " users in room #{$this->id}.\n");
+  }
+
+  // 退室
+  public function leave($user)
+  {
+    unset($this->users[$user->address]);
+    echo("[leave] {$user->address} left from #{$this->id}\n");
+    echo(count($this->users) . " users in room #{$this->id}.\n");
+  }
+
+  // メッセージ送信
+  public function send($address, $message)
+  {
+    foreach ($this->users as $user) {
+      if ($user->address !== $address) {
+        $user->send($message);
+        echo("[send] $address sent '$message' to {$user->address}\n");
+      }
     }
-
-    // 入室
-    public function join($user)
-    {
-        $this->users[$user->address] = $user;
-        echo("[join] {$user->address} joined into #{$this->id}\n");
-        echo(count($this->users) . " users in room #{$this->id}.\n");
-    }
-
-    // 退室
-    public function leave($user)
-    {
-        unset($this->users[$user->address]);
-        echo("[leave] {$user->address} left from #{$this->id}\n");
-        echo(count($this->users) . " users in room #{$this->id}.\n");
-    }
-
-    // メッセージ送信
-    public function send($address, $message)
-    {
-        foreach ($this->users as $user) {
-            if ($user->address !== $address) {
-                $user->send($message);
-                echo("[send] $address sent '$message' to {$user->address}\n");
-            }
-        }
-    }
+  }
 }
--- server/Rooms.php
+++ server/Rooms.php
@@ -5,37 +5,37 @@ spl_autoload_register(function ($class_name) {
 
 class Rooms
 {
-    // 全ルーム
-    private $rooms = [];
+  // 全ルーム
+  private $rooms = [];
 
-    // ルームを作成して入室
-    public function join($room_id, $address, $socket)
-    {
-        $room = $this->get_room($room_id);
-        $user = new User($address, $socket);
-        $room->join($user);
-    }
+  // ルームを作成して入室
+  public function join($room_id, $address, $socket)
+  {
+    $room = $this->get_room($room_id);
+    $user = new User($address, $socket);
+    $room->join($user);
+  }
 
-    // 該当のルームから退室
-    public function leave($room_id, $address)
-    {
-        $room = $this->get_room($room_id);
-        $room->leave($address);
-    }
+  // 該当のルームから退室
+  public function leave($room_id, $address)
+  {
+    $room = $this->get_room($room_id);
+    $room->leave($address);
+  }
 
-    // 該当のルームにメッセージを送信
-    public function send($room_id, $message, $address)
-    {
-        $room = $this->get_room($room_id);
-        $room->send($address, $message);
-    }
+  // 該当のルームにメッセージを送信
+  public function send($room_id, $message, $address)
+  {
+    $room = $this->get_room($room_id);
+    $room->send($address, $message);
+  }
 
-    // ルームを取得。なければ作成
-    private function get_room($id)
-    {
-        if (!isset($this->rooms[$id])) {
-            $this->rooms[$id] = new Room($id);
-        }
-        return $this->rooms[$id];
+  // ルームを取得。なければ作成
+  private function get_room($id)
+  {
+    if (!isset($this->rooms[$id])) {
+      $this->rooms[$id] = new Room($id);
     }
+    return $this->rooms[$id];
+  }
 }
