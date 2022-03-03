c1a02077578d3b5b766d4f2c3158c64b71342ced
kaneko <kaneko@mastershen>
2022-03-03 09:32:35 +0900

    各コマンドのログを追加

--- server/Room.php
+++ server/Room.php
@@ -15,20 +15,23 @@ class Room
     public function __construct($room_id)
     {
         $this->id = $room_id;
+        echo('Room #' . $this->id . ' created.' . PHP_EOL);
     }
 
     // 入室
     public function join($user)
     {
         $this->users[$user->address] = $user;
-        echo("join: {$user->address} room_id: {$this->id}\n");
-        echo("room count: " . count($this->users) . "\n");
+        echo("[join] {$user->address} joined into #{$this->id}\n");
+        echo(count($this->users) . " users in room #{$this->id}.\n");
     }
 
     // 退室
     public function leave($user)
     {
         unset($this->users[$user->address]);
+        echo("[leave] {$user->address} left from #{$this->id}\n");
+        echo(count($this->users) . " users in room #{$this->id}.\n");
     }
 
     // メッセージ送信
@@ -37,6 +40,7 @@ class Room
         foreach ($this->users as $user) {
             if ($user->address !== $address) {
                 $user->send($message);
+                echo("[send] $address sent '$message' to {$user->address}\n");
             }
         }
     }
