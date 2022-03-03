88be4a505091f1b500ecb855447dc80e7c1dae16
kaneko <kaneko@mastershen>
2022-03-03 09:56:02 +0900

    インデント調整、JSONパースエラーをクライアントに応答

--- server/User.php
+++ server/User.php
@@ -5,20 +5,20 @@ spl_autoload_register(function ($class_name) {
 
 class User
 {
-    public $address;
-    private $socket;
+  public $address;
+  private $socket;
 
-    // constractor
-    public function __construct($address, $socket)
-    {
-        $this->address = $address;
-        $this->socket = $socket;
-    }
+  // constractor
+  public function __construct($address, $socket)
+  {
+    $this->address = $address;
+    $this->socket = $socket;
+  }
 
-    public function send($message)
-    {
-        $json = json_encode(['jsonrpc' => 2.0, 'method' => 'send', 'params' => $message]);
-        stream_socket_sendto($this->socket, $json, 0, $this->address);
-        echo("send: $json to: {$this->address}\n");
-    }
+  public function send($message)
+  {
+    $json = json_encode(['jsonrpc' => 2.0, 'method' => 'send', 'params' => $message]);
+    stream_socket_sendto($this->socket, $json, 0, $this->address);
+    echo("send: $json to: {$this->address}\n");
+  }
 }
--- server/main.php
+++ server/main.php
@@ -9,14 +9,14 @@ const RPC_METHOD_SEND = "send";
 
 // help
 if($argc != 3){
-	echo <<< EOL
+  echo <<< EOL
 usage: php main.php [address] [port]
 
     address      : Listenするアドレス
     port         : Listenするポート番号
 
 EOL;
-	exit(1);
+  exit(1);
 }
 
 // パラメータ
@@ -29,25 +29,31 @@ $rooms = new Rooms();
 // サーバソケット作成
 $socket = stream_socket_server("udp://$listen_address:$listen_port", $errno, $errstr, STREAM_SERVER_BIND);
 if (!$socket) {
-    die("$errstr ($errno)");
+  die("$errstr ($errno)");
 }
-echo('Start to listen.');
+echo('Start to listen.' . PHP_EOL);
 
 // 受信ループ
 do {
-	$json = stream_socket_recvfrom($socket, 1500, 0, $address);
-	echo($json);
-    $data = json_decode($json);
-    switch($data->method){
-      case RPC_METHOD_JOIN:
-        $rooms->join($data->params->room_id, $address, $socket);
-        break;
-      case RPC_METHOD_LEAVE:
-        $rooms->leave($data->params->room_id, $address);
-        break;
-      case RPC_METHOD_SEND:
-        $rooms->send($data->params->room_id, $data->params->message, $address);
-        break;
-    }
+  $json = stream_socket_recvfrom($socket, 1500, 0, $address);
+  echo($json);
+  $data = json_decode($json);
+  if(!$data){
+    echo("invalid json: $json\n");
+    $json = json_encode(['jsonrpc' => 2.0, 'error' => "invalid json: $json"]);
+    stream_socket_sendto($socket, $json, 0, $address);
+    continue;
+  }
+  switch($data->method){
+    case RPC_METHOD_JOIN:
+      $rooms->join($data->params->room_id, $address, $socket);
+      break;
+    case RPC_METHOD_LEAVE:
+      $rooms->leave($data->params->room_id, $address);
+      break;
+    case RPC_METHOD_SEND:
+      $rooms->send($data->params->room_id, $data->params->message, $address);
+      break;
+  }
 } while ($json !== false);
 
